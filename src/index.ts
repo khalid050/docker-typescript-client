efc47675d1970157d42ed07f14ecb123d15a2111
Khalid Umar <khalid.umar101@gmail.com>
2023-01-07 01:05:02 -0500

    Delete archive folder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import Container from "./container";
 import Image from "./image";
-import fs from "fs";
 async function main() {
-  // const builtImage = await Image.build(`${process.cwd()}/src/archive`, {t: 'myimage'});
-  // console.log({ builtImage });
-  // const images = await Image.list();
-  // console.log({ images });
-  const u = await Image.inspect('myimage');
-  console.log({u})
 }
 
 (async function () {
